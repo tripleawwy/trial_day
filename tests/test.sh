62ecf1ec7a0275d0918af0872e6fa656df7bac89
tripleawwy <bjoern.marten@hotmail.de>
2020-09-19 01:15:18 +0200

    Remove directory dependancy and add 'some verbosity'

--- tests/test.sh
+++ tests/test.sh
@@ -1,10 +1,18 @@
 #!/usr/bin/bash
 
-HOST1="root"
-HOST2="ipoque"
-HOST3="docker-user"
+HOST="localhost"
 
-ssh $HOST1 -q -o "BatchMode=yes" "echo 2>&1" && echo $HOST1 SSH_OK || echo $HOST1 SSH_NOK
-ssh $HOST2 -q -o "BatchMode=yes" "echo 2>&1" && echo $HOST2 SSH_OK || echo $HOST2 SSH_NOK
-ssh $HOST3 -q -o "BatchMode=yes" "echo 2>&1" && echo $HOST3 SSH_OK || echo $HOST3 SSH_NOK
+USER1="root@$HOST"
+USER2="ipoque@$HOST"
+USER3="docker-user@$HOST"
+
+
+data_dir="$(dirname $(dirname $(realpath $0)))"
+key1_dir=$data_dir/docker/root/.ssh/root-user
+key2_dir=$data_dir/roles/ssh/files/public_keys/origin
+key3_dir=$data_dir/roles/ssh/files/public_keys/docker-user-key
+
+ssh -q -p 2222 -o "BatchMode=yes" $USER1 -i $key1_dir "echo 2>&1" && echo $USER1 SSH_ENABLED || echo $USER1 SSH_DISABLED
+ssh -q -p 2222 -o "BatchMode=yes" $USER2 -i $key2_dir "echo 2>&1" && echo $USER2 SSH_ENABLED || echo $USER2 SSH_DISABLED
+ssh -q -p 2222 -o "BatchMode=yes" $USER3 -i $key3_dir "echo 2>&1" && echo $USER3 SSH_ENABLED || echo $USER3 SSH_DISABLED
 
